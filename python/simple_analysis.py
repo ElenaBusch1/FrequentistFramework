e1a647f3e9bb02394ad08408c1e22492edcb4674
Emma Tolley <emma.tolley@cern.ch>
2020-05-15 14:31:36 +0200

    test code dev

--- python/simple_analysis.py
+++ python/simple_analysis.py
@@ -4,7 +4,8 @@ def getHistogram(datafilename,datahistname ):
     datafile = ROOT.TFile(datafilename,"READ")
     datahist = datafile.Get(datahistname)
     datahist.SetDirectory(0)
-
+    #datahist.Scale(1, "width")
+    datahist.Sumw2(ROOT.kTRUE);
     datafile.Close()
     return datahist
 
@@ -20,10 +21,10 @@ def getJ100Histogram():
 
 def setBkfFunc_UA2(w):
     # fit parameters
-    w.factory("p1[0.1, -0.5, 1.5]") 
-    w.factory("p2[4.5, -10, 10]") 
-    w.factory("p3[21, -30, 20]") 
-    w.factory("p4[-28, -40, 10]") 
+    w.factory("p1[0.1, -100, 100]") 
+    w.factory("p2[4.5, -100, 10]") 
+    w.factory("p3[21, -100, 100]") 
+    w.factory("p4[-28, -200, 200]") 
     # background function
     w.factory("EXPR::background('@1*TMath::Power(@0/13000.,-1*@2)*TMath::Exp( -1*(@3*@0/13000. + @4*TMath::Power(@0/13000.,2)))',{mass,p1,p2,p3,p4})")
 
@@ -36,6 +37,56 @@ def setBkfFunc_4param(w):
     # background function
     w.factory("EXPR::background('@1*TMath::Power(1-@0/13000.,1*@2)*1/TMath::Power( @0/13000., @3 + @4*TMath::Log(@0/13000.))',{mass,p1,p2,p3,p4})")
 
+def makePlot(frame, frame2, frame3, datahist):
+    c = ROOT.TCanvas("c1","c1",600,600)
+    pad1 = ROOT.TPad("pad1", "pad1", 0, 0.5, 1, 1.0)
+    pad1.SetBottomMargin(0) #Upper and lower plot are joined
+    pad1.Draw()
+
+    pad1.cd()
+    #pad1.SetLeftMargin(0.15)
+    #frame.GetYaxis().SetTitleOffset(1.6)
+    frame.Draw()
+    pad1.SetLogy()
+
+    datahist.SetMarkerColor(ROOT.kBlue);
+    datahist.SetLineColor(ROOT.kBlue);
+    datahist.SetMarkerStyle(ROOT.kOpenCircle);
+    datahist.SetMarkerSize(1.5);
+    datahist.Draw("sameP");
+
+    c.cd()
+    pad2 = ROOT.TPad("pad2", "pad2", 0, 0.3, 1, 0.5);
+    pad2.SetTopMargin(0);
+    pad2.SetBottomMargin(0.0)
+    #pad2.SetLeftMargin(0.15)
+    pad2.Draw()
+    pad2.cd()
+    #frame2.GetYaxis().SetTitleOffset(1.6)
+    frame2.GetYaxis().SetTitle("Residual Distribution")
+    frame2.GetYaxis().SetLabelSize(0.1)
+    frame2.GetYaxis().SetTitleOffset(0.3)
+    frame2.GetYaxis().SetTitleSize(0.1)
+    frame2.Draw()
+
+    c.cd()
+    pad3 = ROOT.TPad("pad3", "pad3", 0, 0.1, 1, 0.3);
+    pad3.SetTopMargin(0);
+    pad3.SetBottomMargin(0.1)
+    #pad3.SetLeftMargin(0.15)
+    pad3.Draw()
+    pad3.cd()
+    #pad3.SetLeftMargin(0.15)
+    frame3.GetYaxis().SetTitleOffset(0.3)
+    frame3.GetYaxis().SetTitle("Pull Distribution")
+    frame3.GetXaxis().SetTitle("Mass [GeV]")
+    frame3.GetXaxis().SetLabelSize(0.1)
+    frame3.GetXaxis().SetTitleSize(0.1)
+    frame3.GetYaxis().SetLabelSize(0.1)
+    frame3.GetYaxis().SetTitleSize(0.1)
+    frame3.Draw()
+
+    raw_input("Press Enter...")
 
 
 if __name__ == '__main__':
@@ -45,21 +96,21 @@ if __name__ == '__main__':
 
     #### Set input data & fit range ####
     #observable mass with fit range
-    #w.factory("mass[400,2079]")
-    #datahist = getJ75Histogram()
-    #mytitle = "J75 Data"
+    w.factory("mass[400,2079]")
+    datahist = getJ75Histogram()
+    mytitle = "J75 Data"
 
     #For the 1400 GeV mass point we fit bins 22 through 40 which corresponds to a mass range of 927-1998 GeV.
     #for the 800 GeV Z' mass, we fit from bins 9  to 27 in our spectrum, which corresponds to a mass range of 531 GeV to 1186 GeV.
 
 
-    w.factory("mass[ 1300,2079]")
-    datahist = getJ100Histogram()
-    mytitle = "J100 Data, fit range = [ 927,2079]"
+    #w.factory("mass[ 2000,2079]")
+    #datahist = getJ100Histogram()
+    #mytitle = "J100 Data, fit range = [ 927,2079]"
 
     #### Set up bkg function ###########
     #setBkfFunc_UA2(w)
-    setBkfFunc_4param(w)
+    setBkfFunc_UA2(w)
 
     ####################################
 
@@ -68,54 +119,49 @@ if __name__ == '__main__':
     # workaround to call import function (import is a protected keyword in python)
     getattr(w,'import')(data, ROOT.RooFit.Rename("data"))
 
+    
     # set up frame object
     frame = w.var('mass').frame(ROOT.RooFit.Title(mytitle)) ;
 
-    # fitTo
-    w.pdf("background").fitTo(data, ROOT.RooFit.Strategy(1),  ROOT.RooFit.SumW2Error(True), ROOT.RooFit.Minimizer("Minuit2","Migrad")) ;
 
+    nll = w.pdf("background").createNLL(data)
+    minim = ROOT.RooMinimizer(nll);
+    minim.minimize("Minuit2")
+
+    # fitTo
+    #nll.fitTo(data, ROOT.RooFit.Strategy(1),  ROOT.RooFit.SumW2Error(True), ROOT.RooFit.Minimizer("Minuit2","Migrad")) ;
+    
 
     w.data("data").plotOn(frame) ;
     w.pdf("background").plotOn(frame) ;
-    w.pdf("background").paramOn(frame,ROOT.RooFit.Layout(0.55)) ;
+    w.pdf("background").paramOn(frame,ROOT.RooFit.Layout(0.65)) ;
 
 
-    #Construct a histogram with the residuals of the data w.r.t. the curve
+    # Construct a histogram with the residuals of the data w.r.t. the curve
     hresid = frame.residHist() 
 
-    #Construct a histogram with the pulls of the data w.r.t the curve
+    # Construct a histogram with the pulls of the data w.r.t the curve
     hpull = frame.pullHist() 
 
-    #Create a new frame to draw the residual distribution and add the distribution to the frame
-    frame2 = w.var('mass').frame(ROOT.RooFit.Title("Residual Distribution")) ;
+    # Create a new frame to draw the residual distribution and add the distribution to the frame
+    frame2 = w.var('mass').frame(ROOT.RooFit.Title("")) ;
     frame2.addPlotable(hresid,"P") ;
 
-    #Create a new frame to draw the pull distribution and add the distribution to the frame
-    frame3 = w.var('mass').frame(ROOT.RooFit.Title("Pull Distribution")) ;
+    # Create a new frame to draw the pull distribution and add the distribution to the frame
+    frame3 = w.var('mass').frame(ROOT.RooFit.Title("")) ;
     frame3.addPlotable(hpull,"P") ;
 
-    c = ROOT.TCanvas("c1","c1",900,300)
-    c.Divide(3) 
-    #c.cd(1)
-
-    c.cd(1)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame.GetYaxis().SetTitleOffset(1.6)
-    frame.Draw()
-    ROOT.gPad.SetLogy()
-
-    c.cd(2)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame2.GetYaxis().SetTitleOffset(1.6)
-    frame2.Draw()
-
-    c.cd(3)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame3.GetYaxis().SetTitleOffset(1.6)
-    frame3.Draw()
+    makePlot(frame, frame2, frame3, datahist)
 
     w.Print()
 
-    raw_input("Press Enter...")
+    #ROOT.SetOwnership(can, False)
+
+    #ROOT.SetOwnership(pad, False)
+
+    #curve = frame.getCurve("background");'''
+
+
+    
 
 
