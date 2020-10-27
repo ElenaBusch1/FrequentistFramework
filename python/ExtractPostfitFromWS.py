5a1f73f2b125f68b6c0010671517149970a2072a
Falk Bartels <falk.bartels@cern.ch>
2020-10-27 16:41:57 +0100

    Added some python scripts to make pseudo data

---
+++ python/ExtractPostfitFromWS.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+import ROOT
+import sys, re, os, math, argparse
+from ROOT import *
+
+def main(args):
+
+    parser = argparse.ArgumentParser(description='%prog [options]')
+    parser.add_argument('--datafile', dest='datafile', type=str, default='../Input/data/dijetTLAnlo/data_J75yStar03_range400_2079.root', help='original data file name (to get binning from)')
+    parser.add_argument('--datahist', dest='datahist', type=str, default='data', help='original data hist name (to get binning from)')
+    parser.add_argument('--datafirstbin', dest='datafirstbin', type=int, default=0, help='First bin in data histogram considered in fit. 0 for first non-underflow bin')
+    parser.add_argument('--wsfile', dest='wsfile', type=str, default='../run/FitResult_mean500_width7.root', help='Workspace file name')
+    parser.add_argument('--wsname', dest='wsname', type=str, default='combWS', help='Name of workspace')
+    parser.add_argument('--modelname', dest='modelname', type=str, default='ModelConfig', help='Name of model in workspace')
+#    parser.add_argument('--outfile', dest='outfile', type=str, default='../run/', help='Output file name')
+
+    args = parser.parse_args(args)
+
+    f = ROOT.TFile(args.wsfile, "READ")
+    w = f.Get(args.wsname)
+    
+#    w.Print("all")
+
+    fd =  ROOT.TFile(args.datafile, "READ")
+    h_data = fd.Get(args.datahist)
+    
+    model = w.obj(args.modelname)
+    print "Observed:"
+
+    pdf = model.GetPdf()
+
+    cat = pdf.indexCat()
+    nChan = cat.numBins("")
+
+    print "There are %d channels" % nChan
+
+    obs = pdf.getObservables(model.GetObservables())
+    obs.Print()
+    
+    data = w.data("combData")
+    dataList = data.split( cat, True )
+
+    # asdata = w.data("asimovData_0")
+    # asdata.Print("V")
+    # asdata.get(0).Print("V")
+    # asdata.get(1).Print("V")
+
+    for i in range(nChan):
+    
+        datai = dataList.At( i )
+        channelname = cat.getLabel()
+        pdfi = pdf.getPdf(channelname)
+        x = pdfi.getObservables(datai).first()
+        
+        print "Expected:"
+        # print type(x)
+        expectedEvents = pdfi.expectedEvents(RooArgSet(x))
+    
+        print expectedEvents
+    
+        hpdf = pdfi.createHistogram("hpdf", x)
+        hpdf.Scale(expectedEvents/hpdf.Integral())
+
+        h_postfit = h_data.Clone()
+        h_postfit.Reset("M")
+        
+        for ibin in range(1, hpdf.GetNbinsX()+1):
+            h_postfit.SetBinContent(ibin + args.datafirstbin, hpdf.GetBinContent(ibin))
+            h_postfit.SetBinError(ibin + args.datafirstbin, 0)
+
+        # h_postfit.Draw()
+
+        h_residuals = h_data.Clone()
+        h_residuals.Reset("M")
+
+        
+
+        for ibin in range(1, h_residuals.GetNbinsX()+1):
+            valueErrorData = h_data.GetBinError(ibin)
+            valueData = h_data.GetBinContent(ibin)
+            postFitValue = h_postfit.GetBinContent(ibin)
+
+            binSig = 0.0  
+            if valueErrorData > 0.0:
+                binSig = (valueData - postFitValue)/valueErrorData
+
+            h_residuals.SetBinContent(ibin, binSig)
+            h_residuals.SetBinError(ibin, 0)
+
+        fout = ROOT.TFile("postfit_%s.root" % channelname, "RECREATE")
+        h_postfit.Write("postfit")
+        h_data.Write("data")
+        h_residuals.Write("postFitSigma")
+        fout.Close()
+        # raw_input("press enter")
+
+if __name__ == "__main__":  
+   sys.exit(main(sys.argv[1:]))   
---
+++ python/generatePseudoData.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import ROOT
+import sys, re, os, math, argparse
+
+gRand = ROOT.TRandom3()
+
+def fluctuatePoisson(hist):
+    global gRand
+
+    result = hist.Clone("fluctuated hist");
+    result.Reset("M")
+
+    nBinsX = hist.GetNbinsX();
+    nBinsY = hist.GetNbinsY();
+
+    for i in range(0, nBinsX):
+        for j in range(0, nBinsY):
+            ibin = hist.GetBin(i, j);
+            fluc = gRand.Poisson(hist.GetBinContent(ibin));
+            if fluc >= 0:
+                result.SetBinContent(ibin, fluc);
+                result.SetBinError(ibin, math.sqrt(fluc));
+
+    return result;
+
+
+def main(args):
+    global gRand
+
+    parser = argparse.ArgumentParser(description='%prog [options]')
+    parser.add_argument('--infile', dest='infile', type=str, default='', help='Input file name')
+    parser.add_argument('--inhist', dest='inhist', type=str, default='', help='Input hist name')
+    parser.add_argument('--outfile', dest='outfile', type=str, default='', help='Output file name')
+    parser.add_argument('--outhist', dest='outhist', type=str, default='', help='Output hist name')
+    parser.add_argument('--nreplicas', dest='nreplicas', type=int, default=50, help='Number of replicas to generate')
+    parser.add_argument('--scaling', dest='scaling', type=float, default=1., help='factor to scale the lumi by')
+    
+    args = parser.parse_args(args)
+
+    f_in = ROOT.TFile(args.infile, "READ")
+    h_in = f_in.Get(args.inhist)
+    h_in.SetDirectory(0)
+    f_in.Close()
+
+    h_in.Scale(args.scaling)
+
+    f_out = ROOT.TFile(args.outfile, "RECREATE")
+    f_out.cd()
+
+    for i in range(0, args.nreplicas):
+        if (i%(args.nreplicas/20) == 0):
+            print i,"/",args.nreplicas
+
+        gRand.SetSeed(i)
+        h_out = fluctuatePoisson(h_in)
+        h_out.Write("%s_%d" % (args.outhist, i))
+
+    f_out.Close()
+
+if __name__ == "__main__":  
+   sys.exit(main(sys.argv[1:]))   
