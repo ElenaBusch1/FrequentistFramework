8eda39dc1213864d2c6bbc3e6ae6814c0ca2a514
Falk Bartels <falk.bartels@cern.ch>
2021-12-09 10:43:56 +0100

    Switching createCoverageGraph.py to list of files instead of glob string

--- python/createCoverageGraph.py
+++ python/createCoverageGraph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import ROOT
-import sys, re, os, math, argparse
+import sys, re, os, math, optparse
 from array import array
 from ROOT import *
 from math import sqrt
@@ -10,86 +10,108 @@ gROOT.LoadMacro("../atlasstyle-00-04-02/AtlasLabels.C")
 gROOT.LoadMacro("../atlasstyle-00-04-02/AtlasStyle.C")
 gROOT.LoadMacro("../atlasstyle-00-04-02/AtlasUtils.C")
 
-
-# sigmeans=[ 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1300, 1400, 1500, 1600, 1700, 1800, ]
-# sigwidths=[ 5, 7, 10, 12, 15, ]
-sigmeans=[ 700, 1000, 1400, 1800 ]
-# sigmeans=[ 1000 ]
-sigwidths=[ 5 ]
-# sigamps=[ 10, 7, 5, 3, 0 ]
-sigamps=[ 5, 4, 3, 2, 1, 0 ]
-
-# sigamps=[ 0 ]
-
 lumi = 130000
 
 def main(args):
     SetAtlasStyle()
  
-    parser = argparse.ArgumentParser(description='%prog [options]')
-    parser.add_argument('--infile', dest='infile', type=str, default='/data/scratch/users/bartels/FarmOutput/TLA/quickFit_injections_globalFit_fourpar_201122/quickFit_swift_J100_${MEAN}_${WIDTH}_${AMP}_sbFit/output.*/run/Limits_*.txt', help='Input FitResult paths')
-    parser.add_argument('--outfile', dest='outfile', type=str, default='extractionGraphs.root', help='Output file name')
+    parser = optparse.OptionParser(description='%prog [options] INPUT')
+    parser.add_option('--outfile', dest='outfile', type=str, default='limitGraphs.root', help='Output file name')
     
-    args = parser.parse_args(args)
+    options, args = parser.parse_args(args)
 
-    path_limits = args.infile
-    path_injection = path_limits.replace("Limits", "PD").replace(".txt", ".root")
+    paths = args
+    sigmeans = set()
+    sigwidths = set()
+    sigamps = set()
+    dict_file = {}
+
+    for p in paths:
+        res=re.search(r'mean(\d+)_width(\d+)(:?_amp\d+)?', p)
+        m=int(res.group(1))
+        w=int(res.group(2))
+        
+        sigmeans.add(m)
+        sigwidths.add(w)
+
+        try:
+            a=int(res.group(3)[4:])
+        except:
+            a=0
+        sigamps.add(a)
+            
+        if (m, w, a) in dict_file:
+            dict_file[(m, w, a)].append(p)
+        else:
+            dict_file[(m, w, a)]=[p]
+            
+    sigmeans = list(sigmeans)
+    sigwidths = list(sigwidths)
+    sigamps = list(sigamps)
+
+    sigmeans.sort()
+    sigwidths.sort()
+    sigamps.sort()
 
     colors = [kBlue, kRed+1, kOrange-3]
 
-    fout = TFile(args.outfile, "RECREATE")
+    fout = TFile(options.outfile, "RECREATE")
 
     for i,sigwidth in enumerate(sigwidths):
         
         for j,sigmean in enumerate(sigmeans):
             
             g = TGraph()
+            g_exp = TGraph()
+            g_exp_2u = TGraph()
+            g_exp_1u = TGraph()
+            g_exp_1d = TGraph()
+            g_exp_2d = TGraph()
             sqrtB = None
 
             for k,sigamp in enumerate(sigamps):
     
                 #find number of injected events:
               
+                try:
+                    tmp_path_limits = dict_file[(sigmean, sigwidth, sigamp)]
+                except:
+                    print "WARNING: No limit file for", sigmean, sigwidth, sigamp
+                    continue
+
                 if sigamp > 0:
-                    tmp_path_injection = path_injection
-                    tmp_path_injection = tmp_path_injection.replace("${MEAN}", str(sigmean))
-                    tmp_path_injection = tmp_path_injection.replace("${WIDTH}", str(sigwidth))
-                    tmp_path_injection = tmp_path_injection.replace("${AMP}", str(sigamp))
-                    print tmp_path_injection 
-                    tmp_path_injection = glob(tmp_path_injection)
+                    tmp_path_injection = tmp_path_limits.replace("Limits", "PD").replace(".txt", ".root")
 
-                    if len(tmp_path_injection) == 0:
-                        continue
-
-                    f = TFile(tmp_path_injection[0])
-                    h = f.Get("pseudodata_0_injection")
-                    n_injected = h.Integral(0, h.GetNbinsX()+1)
-                    f.Close()
+                    try:
+                        f = TFile(tmp_path_injection[0])
+                        h = f.Get("pseudodata_0_injection")
+                        n_injected = h.Integral(0, h.GetNbinsX()+1)
+                        f.Close()
+                    except:
+                        print "WARNING: Could not find injection file for tmp_path_limits. Using n_injected=0 now."
+                        n_injected = 0
                 else:
                     n_injected = 0
 
                 if sqrtB == None:
                     sqrtB = (n_injected / sigamp) if sigamp != 0 else 1
-                    print "setting sqrtB to", sqrtB
+                    # print "setting sqrtB to", sqrtB
                 
-                tmp_path_limits = path_limits
-                tmp_path_limits = tmp_path_limits.replace("${MEAN}", str(sigmean))
-                tmp_path_limits = tmp_path_limits.replace("${WIDTH}", str(sigwidth))
-                tmp_path_limits = tmp_path_limits.replace("${AMP}", str(sigamp))
-                print tmp_path_limits
-                tmp_path_limits = glob(tmp_path_limits)
-                if len(tmp_path_limits) == 0:
-                    continue
-
                 inj_limit = []
                 nans = 0
 
                 for path in tmp_path_limits:
                     with open(path) as f:
-                        limit = float(f.readline().split()[0])
+                        limits = f.readline().split()
+                        limit = float(limits[0])
+                        limit_exp = float(limits[1])
+                        limit_exp2u = float(limits[2])
+                        limit_exp1u = float(limits[3])
+                        limit_exp1d = float(limits[4])
+                        limit_exp2d = float(limits[5])
                         
                     # print n_injected, limit
-                    inj_limit.append((n_injected, limit))
+                    inj_limit.append((n_injected, limit, limit_exp, limit_exp2u, limit_exp1u, limit_exp1d, limit_exp2d))
                     if math.isnan(limit):
                         nans += 1
                 
@@ -97,13 +119,33 @@ def main(args):
                 # if float(nans) / len(inj_limit) < 0.02:
                 for t in inj_limit:
                     g.SetPoint(g.GetN(), t[0]/sqrtB, t[1]/sqrtB)
+                    g_exp.SetPoint(g_exp.GetN(), t[0]/sqrtB, t[2]/sqrtB)
+                    g_exp_2u.SetPoint(g_exp_2u.GetN(), t[0]/sqrtB, t[3]/sqrtB)
+                    g_exp_1u.SetPoint(g_exp_1u.GetN(), t[0]/sqrtB, t[4]/sqrtB)
+                    g_exp_1d.SetPoint(g_exp_1d.GetN(), t[0]/sqrtB, t[5]/sqrtB)
+                    g_exp_2d.SetPoint(g_exp_2d.GetN(), t[0]/sqrtB, t[6]/sqrtB)
                 # else:
                 #     print "skipping", inj_limit[0][0], "due to NaNs"
-
    
             fout.cd()
+
             g.SetTitle("%d GeV Gauss (%d%%)" % (sigmean, sigwidth))
             g.Write("g1_limit_gauss_%d_%d" % (sigmean, sigwidth))
+
+            g_exp.SetTitle("%d GeV Gauss (%d%%)" % (sigmean, sigwidth))
+            g_exp.Write("g1_exp_limit_gauss_%d_%d" % (sigmean, sigwidth))
+
+            g_exp_2u.SetTitle("%d GeV Gauss (%d%%)" % (sigmean, sigwidth))
+            g_exp_2u.Write("g1_exp_2u_limit_gauss_%d_%d" % (sigmean, sigwidth))
+
+            g_exp_1u.SetTitle("%d GeV Gauss (%d%%)" % (sigmean, sigwidth))
+            g_exp_1u.Write("g1_exp_1u_limit_gauss_%d_%d" % (sigmean, sigwidth))
+
+            g_exp_1d.SetTitle("%d GeV Gauss (%d%%)" % (sigmean, sigwidth))
+            g_exp_1d.Write("g1_exp_1d_limit_gauss_%d_%d" % (sigmean, sigwidth))
+
+            g_exp_2d.SetTitle("%d GeV Gauss (%d%%)" % (sigmean, sigwidth))
+            g_exp_2d.Write("g1_exp_2d_limit_gauss_%d_%d" % (sigmean, sigwidth))
 
 
     fout.Close()
