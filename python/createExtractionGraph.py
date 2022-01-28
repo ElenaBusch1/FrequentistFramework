f713f771cc893926bf450ec46b6d45cc46519d3c
Falk Bartels <falk.bartels@cern.ch>
2022-01-28 12:26:27 +0100

    generalised createExtractionGraph.py

--- python/createExtractionGraph.py
+++ python/createExtractionGraph.py
@@ -14,19 +14,6 @@ gROOT.LoadMacro("../atlasstyle-00-04-02/AtlasStyle.C")
 gROOT.LoadMacro("../atlasstyle-00-04-02/AtlasUtils.C")
 
 
-# sigmeans=[ 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1300, 1400, 1500, 1600, 1700, 1800, ]
-# sigwidths=[ 5, 7, 10, 12, 15, ]
-# sigmeans=[ 700, 800, 1000, 1200]
-sigmeans=[ 700, 1000, 1400, 1800]
-# sigmeans=[ 1000 ]
-sigwidths=[ 5 ]
-# sigamps=[ 0, 3, 5, 7, 10 ]
-# sigamps=[ 10, 7, 5, 3, 0 ]
-sigamps=[ 5, 4, 3, 2, 1, 0 ]
-
-# # colors = [kBlue, kRed+1, kOrange-3]
-# colors = [kMagenta+3, kRed+1, kOrange-3, kSpring+5, kTeal+5, kCyan-1, kAzure-6]
-colors = getColorSteps(len(sigmeans))
 
 def main(args):
     SetAtlasStyle()
@@ -69,6 +56,8 @@ def main(args):
     sigwidths.sort()
     sigamps.sort()
 
+    colors = getColorSteps(len(sigmeans)*len(sigwidths))
+
     fout = TFile(options.outfile, "RECREATE")
 
     profile_list = []
@@ -110,7 +99,8 @@ def main(args):
                 
                 parNames = {}
                 parLists = {}
-                
+                parHists = {}
+        
                 for path in tmp_path_fitresult:
                     try:
                         f = TFile(path)
@@ -124,11 +114,23 @@ def main(args):
                     except:
                         print "Couldn't read fit parameters from", path
                         continue
-                        
-                    # print n_injected, nsig
-                    inj_extr.append((n_injected, nsig))
-                    if math.isnan(nsig):
-                        nans += 1
+
+                for i in parNames:
+                    par = parNames[i]
+                    l = parLists[i]
+                    h = TH1D(par, par, 100, min(l), max(l))
+                    
+                    for e in l:
+                        h.Fill(e)
+
+                    parHists[i] = h
+                    
+                    if "nsig" in par:
+                        # print n_injected, nsig
+                        for e in l:
+                            inj_extr.append((n_injected, e))
+                            if math.isnan(e):
+                                nans += 1
                     
                 print "n_injected: %d,   NaNs: %d" % (n_injected, nans)
                 # if float(nans) / len(inj_extr) < 0.02:
@@ -142,10 +144,14 @@ def main(args):
                 if sqrtB == None:
                     sqrtB = (n_injected / sigamp) if sigamp != 0 else 1
                 
-
                 g_profile.SetPoint(g_profile.GetN(), sigamp, nFit / sqrtB)
                 g_profile.SetPointError(g_profile.GetN()-1, 0, nFitErr / sqrtB)
 
+                d = fout.mkdir("hists_mean%d_width%d_amp%d" % (sigmean, sigwidth, sigamp))
+                d.cd()
+                for h in parHists.values():
+                    h.Write()
+                d.Close()
                 # print "Setting point at", sigamp, nFit / sqrtB
 
             fout.cd()
@@ -187,19 +193,19 @@ def main(args):
     l.Draw()
 
     lumi = 29
-    if "lumi" in path_fitresult:
+    if "lumi" in dict_file.values()[0]:
         try:
-            lumi=int(path_fitresult.split("lumi")[-1].split("_")[0])
+            lumi=int(dict_file.values()[0].split("lumi")[-1].split("_")[0])
         except:
             pass
     text1 = "Pseudodata %d fb^{-1}" % lumi
 
     text2 = "global fit"
-    if "four" in  path_fitresult:
+    if "four" in  dict_file.values()[0]:
         text2 += " 4 par"
-    if "five" in  path_fitresult:
+    if "five" in  dict_file.values()[0]:
         text2 += " 5 par"
-    if "nloFit" in path_fitresult:
+    if "nloFit" in dict_file.values()[0]:
         text2 = "NLOFit"
 
     text = text1 + ", " + text2
@@ -207,7 +213,7 @@ def main(args):
     ATLASLabel(0.2, 0.9, "   Work in progress", 13)
     myText(0.2, 0.82, 1, text)
 
-    # raw_input("enter")
+    raw_input("enter")
 
     c.Print(options.outfile.replace(".root", ".png"))
     
