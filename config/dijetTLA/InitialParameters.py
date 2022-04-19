a3c43425fe239ddb665cf0343de9e993ec4ae403
Mariana Toscani <mariana.toscani@cern.ch>
2022-04-19 21:33:12 +0200

    Card cleanup for zprimes

--- config/dijetTLA/InitialParameters.py
+++ config/dijetTLA/InitialParameters.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
 initialParameters = {
-  'partialDataset' : { # Partial dataset (1st unblinding)
+    'TLA2016' : { 
+	
+	'J100' : {  	
+	  
+	  'nbkg' 	: "2E8,0,3E8",
+	  'datafile'	: "Input/data/dijetTLA/lookInsideTheBoxWithUniformMjj.root",
+	  'datahist'	: "Nominal/DSJ100yStar06_TriggerJets_J100_yStar06_mjj_finebinned_all_data",
+	  # Analysis range		
+	  'low'  : 531 ,
+	  'high' : 2997,
+	},
+    },
+    'partialDataset' : { # Partial datafile (1st unblinding)
 	
 	'J100' : {  	
 	  
 	  'nbkg' 	: "4E7,0,1E8",
-	  'dataset'	: "Input/data/dijetTLA/unblinding1_mjj_spectra.root",
+	  'datafile'	: "Input/data/dijetTLA/unblinding1_mjj_spectra.root",
 	  'datahist'	: "L1J100/Mjj_1GeVbinning",
 	  # Analysis range		
-	  'low'  :457 ,
+	  'low'  : 457,
 	  'high' : 2997,
 	},
 	
 	'J40'  : {  
 	  
 	  'nbkg'	: "1E7,0,2E7",
-	  'dataset'	: "Input/data/dijetTLA/unblinding1_mjj_spectra.root",
+	  'datafile'	: "Input/data/dijetTLA/unblinding1_mjj_spectra.root",
 	  'datahist'	: "L1J40/Mjj_1GeVbinning",                      	  
 	  'low'  : 302,
 	  'high' : 1516, 
@@ -25,7 +37,7 @@ initialParameters = {
 	'J50'  : {	
 	  
 	  'nbkg' : "1E7,0,2E7",
-	  'dataset'	: "Input/data/dijetTLA/unblinding1_mjj_spectra.root",
+	  'datafile'	: "Input/data/dijetTLA/unblinding1_mjj_spectra.root",
 	  'datahist'	: "L1J50/Mjj_1GeVbinning",
 	  'low'  : 302,
 	  'high' : 1516, 
@@ -33,12 +45,12 @@ initialParameters = {
 
     },
 
-    'full' : {  # Full lumi: total dataset
+    'full' : {  # Full lumi: total datafile
 
 	'J100' : {  	
 	  
 	  'nbkg' 	: "9E8,0,15E8",
-	  'dataset'	: "",
+	  'datafile'	: "",
 	  'datahist'	: "",
 	  'low'  :457 ,
 	  'high' : 2997,
@@ -47,7 +59,7 @@ initialParameters = {
 	'J40'  : {  
 	  
 	  'nbkg'	: "1E7,0,2E7",
-	  'dataset'	: "",
+	  'datafile'	: "",
 	  'datahist'	: "",                      	  
 	  'low'  : 302,
 	  'high' : 1516, 
@@ -56,7 +68,7 @@ initialParameters = {
 	'J50'  : {	
 	  
 	  'nbkg' : "1E7,0,2E7",
-	  'dataset'	: "",
+	  'datafile'	: "",
 	  'datahist'	: "",
 	  'low'  : 302,
 	  'high' : 1516, 
