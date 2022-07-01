93c641077a1a12c1e07192e848a1eb246c428f0f
Falk Bartels <falk.bartels@cern.ch>
2022-07-01 17:51:44 +0200

    Updating .sh scripts to include prefit and folder flag

--- scripts/run_anaFit.sh
+++ scripts/run_anaFit.sh
@@ -3,55 +3,53 @@
 {
     . scripts/setup_buildAndFit.sh
 
-    trig=J100
-    pars=six
+    # for pars in five six seven
+    for pars in five
+    do
+	# for trig in J50Comb J100
+	for trig in J100
+	do
+	    folder=run/outOfTheBoxFit
+	    signalfile=config/dijetTLA/signal/signal_dijetTLA.template
+	    backgroundfile=config/dijetTLA/background_dijetTLA_${trig/Comb/}yStar06_${pars}Par.template
+	    categoryfile=config/dijetTLA/category_dijetTLA.template
+	    topfile=config/dijetTLA/dijetTLA_J100yStar06.template
+	    wsfile=${folder}/dijetTLA_combWS_${pars}Par_${trig}yStar06.root
+	    sigmean=1000
+	    sigwidth=7
+	    dosignal=1
+	    dolimit=1
+	    outputfile=${folder}/FitResult_anaFit_${pars}Par_${trig}yStar06_mean${sigmean}_width${sigwidth}.root
+	    rangelow=457
+	    rangehigh=2997
+	    datafile=Input/data/dijetTLA/unblinding1_mjj_spectra.root
+	    datahist=L1${trig}/Mjj_1GeVbinning
+	    nbkg="dummy" #overwritten by prefit
+	    maskthreshold=0.01
+	    doprefit=1
 
-    signalfile=config/dijetTLA/signal/signal_dijetTLA.template
-    backgroundfile=config/dijetTLA/background_dijetTLA_${trig/Comb/}yStar06_${pars}Par.xml
-    categoryfile=config/dijetTLA/category_dijetTLA.template
-    topfile=config/dijetTLA/dijetTLA_J100yStar06.template
-    wsfile=run/dijetTLA_combWS_swift.root
-    sigmean=1000
-    sigwidth=7
-    dosignal=1
-    dolimit=1
-    outputfile=run/FitResult_swift_${pars}Par_${trig}yStar06_mean${sigmean}_width${sigwidth}.root
-    rangelow=457
-    rangehigh=2997
-    # rangelow=302
-    # rangehigh=1516
-    datafile=Input/data/dijetTLA/unblinding1_mjj_spectra.root
-    datahist=L1${trig}/Mjj_1GeVbinning
-    nbkg="2E8,0,3E8"
-    # datafile=run/PD_Run2_GlobalFit_457_2997_sixPar_finebinned_J100.root
-    # datahist=unfluctuated
-    # nbkg="1.5E9,0,2E9"
-    # datafile=Input/data/dijetTLA/PD_Run2_GlobalFit_302_1516_fivePar_finebinned_J50Comb.root
-    # datahist=unfluctuated
-    # nbkg="1E9,0,2E9"
-    maskthreshold=-1
-    folder=run
+	    flags=""
+	    if (( $dosignal )); then flags="$flags --dosignal"; fi
+	    if (( $dolimit )); then flags="$flags --dolimit"; fi
+	    if (( $doprefit )); then flags="$flags --doprefit"; fi
 
-    flags=""
-    if (( $dosignal )); then flags="$flags --dosignal"; fi
-    if (( $dolimit )); then flags="$flags --dolimit"; fi
-
-    ./python/run_anaFit.py \
-    	--datafile $datafile \
-    	--datahist $datahist \
-    	--signalfile $signalfile \
-    	--backgroundfile $backgroundfile \
-    	--categoryfile $categoryfile \
-    	--topfile $topfile \
-    	--wsfile $wsfile \
-    	--sigmean $sigmean \
-    	--sigwidth $sigwidth \
-    	--nbkg $nbkg \
-    	--rangelow $rangelow \
-    	--rangehigh $rangehigh \
-    	--outputfile $outputfile \
-	--maskthreshold $maskthreshold \
-	--doprefit \
-	--folder $folder \
-        $flags
+	    ./python/run_anaFit.py \
+    		--datafile $datafile \
+    		--datahist $datahist \
+    		--backgroundfile $backgroundfile \
+    		--sigfile $sigfile \
+    		--categoryfile $categoryfile \
+    		--topfile $topfile \
+    		--wsfile $wsfile \
+    		--sigmean $sigmean \
+    		--sigwidth $sigwidth \
+    		--nbkg $nbkg \
+    		--rangelow $rangelow \
+    		--rangehigh $rangehigh \
+    		--outputfile $outputfile \
+		--maskthreshold $maskthreshold \
+		--folder $folder \
+		$flags
+	done
+    done
 }
--- scripts/run_anaFit_flowchart.sh
+++ scripts/run_anaFit_flowchart.sh
@@ -5,22 +5,25 @@ generatePD=true
 {
     . scripts/setup_buildAndFit.sh
 
-    for pars in five six seven
+    # for pars in five six seven
+    for pars in five
     do
 	# for trig in J50Comb J100
 	for trig in J100
 	do
+	    folder=run/outOfTheBoxPD
 	    signalfile=config/dijetTLA/signal/signal_dijetTLA.template
 	    backgroundfile=config/dijetTLA/background_dijetTLA_${trig/Comb/}yStar06_${pars}Par.template
 	    categoryfile=config/dijetTLA/category_dijetTLA.template
 	    topfile=config/dijetTLA/dijetTLA_J100yStar06.template
-	    wsfile=run/dijetTLA_combWS_anaFit_${pars}Par_${trig}yStar06_bkgonly.root
+	    wsfile=${folder}/dijetTLA_combWS_anaFit_${pars}Par_${trig}yStar06_bkgonly.root
 	    sigmean=1000
 	    sigwidth=5
 	    dosignal=0
 	    dolimit=0
+	    doprefit=1
 	    maskthreshold=-1
-	    outputfile=run/FitResult_anaFit_${pars}Par_${trig}yStar06_bkgonly.root
+	    outputfile=${folder}/FitResult_anaFit_${pars}Par_${trig}yStar06_bkgonly.root
 	    rangelow=302
 	    rangehigh=1516
 	    nbkg="1E7,0,2.5E7"
@@ -36,10 +39,10 @@ generatePD=true
 	    then
 		datahist="L1J50_DETA20-J50J(noHLT_j0_perf_ds1_L1J50)/Mjj_1GeVbinning"
 	    fi
-	    folder=generatePD_531_defaultScale
 	    flags=""
 	    if (( $dosignal )); then flags="$flags --dosignal"; fi
 	    if (( $dolimit )); then flags="$flags --dolimit"; fi
+	    if (( $doprefit )); then flags="$flags --doprefit"; fi
 
 	    ./python/run_anaFit.py \
     		--datafile $datafile \
@@ -56,7 +59,6 @@ generatePD=true
     		--rangehigh $rangehigh \
     		--outputfile $outputfile \
 		--maskthreshold $maskthreshold \
-		--doprefit \
 		--folder $folder \
 		$flags
 
@@ -85,10 +87,10 @@ generatePD=true
 		fi
 
 		toys=1000
-		
-		echo python python/generatePseudoData.py --infile ${outputfile/FitResult/PostFit} --inhist J100yStar06/postfit --outhist pseudodata --outfile run/PD_Run2_GlobalFit_${rangelow}_${rangehigh}_${pars}Par_finebinned_${trig}.root --nreplicas $toys --scaling $scalefactor
 
-		python python/generatePseudoData.py --infile ${outputfile/FitResult/PostFit} --inhist J100yStar06/postfit --outhist pseudodata --outfile run/PD_Run2_GlobalFit_${rangelow}_${rangehigh}_${pars}Par_finebinned_${trig}.root --nreplicas $toys --scaling $scalefactor
+		echo python python/generatePseudoData.py --infile ${outputfile/FitResult/PostFit} --inhist J100yStar06/postfit --outhist pseudodata --outfile ${folder}/PD_Run2_GlobalFit_${rangelow}_${rangehigh}_${pars}Par_finebinned_${trig}_scale${scalefactor}.root --nreplicas $toys --scaling $scalefactor
+
+		python python/generatePseudoData.py --infile ${outputfile/FitResult/PostFit} --inhist J100yStar06/postfit --outhist pseudodata --outfile ${folder}/PD_Run2_GlobalFit_${rangelow}_${rangehigh}_${pars}Par_finebinned_${trig}_scale${scalefactor}.root --nreplicas $toys --scaling $scalefactor
 
 	    fi
 	done
