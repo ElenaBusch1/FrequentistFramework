91e8c9ea17909ec6dd3540d38338bcb5d1fc0d10
Falk Bartels <falk.bartels@cern.ch>
2022-07-01 15:42:55 +0200

    Updating template generation python scripts

--- python/PrepareTemplates/unifyBinning.py
+++ python/PrepareTemplates/unifyBinning.py
@@ -2,6 +2,28 @@ import ROOT
 import sys, re, os, math, argparse
 from array import array
 
+def rebinDir(dir_in, dir_out):
+    dir_out.cd()
+
+    for key in dir_in.GetListOfKeys():
+        name = key.GetName()
+        h_in = dir_in.Get(name)
+
+        if isinstance(h_in, ROOT.TDirectory):
+            new_dir_out = dir_out.mkdir(name)
+            rebinDir(h_in, new_dir_out)
+            dir_out.cd()
+            
+        elif isinstance(h_in, ROOT.TH1):
+            h_out = ROOT.TH1D(name, h_in.GetTitle(), h_in.GetNbinsX(), 0, h_in.GetNbinsX())
+
+            for ibin in range(1, h_out.GetNbinsX()+1):
+                h_out.SetBinContent(ibin, h_in.GetBinContent(ibin))
+                h_out.SetBinError(ibin, h_in.GetBinError(ibin))
+
+            h_out.Write(name)
+    
+
 def main(args):
 
     for path in args:
@@ -9,23 +31,8 @@ def main(args):
         f_in = ROOT.TFile(path, "READ")
         f_out = ROOT.TFile(path.replace(".root", "_fixedBins.root"), "RECREATE")
         
-        for histKey in f_in.GetListOfKeys():
-            
-            histName = histKey.GetName()
-            
-            h_in = f_in.Get(histName)
-        
-            if not isinstance(h_in, ROOT.TH1):
-                continue
-        
-            h_out = ROOT.TH1D(histName, h_in.GetTitle(), h_in.GetNbinsX(), 0, h_in.GetNbinsX())
+        rebinDir(f_in, f_out)
 
-            for ibin in range(1, h_out.GetNbinsX()+1):
-                h_out.SetBinContent(ibin, h_in.GetBinContent(ibin))
-                h_out.SetBinError(ibin, h_in.GetBinError(ibin))
-
-            h_out.Write(histName)
-            
         f_out.Close()
 
 if __name__ == "__main__":  
