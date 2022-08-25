b9a7335be93cca8a5a78f42f95c607040d6edab1
Mariana Toscani <mariana.toscani@cern.ch>
2022-08-25 12:30:51 +0200

    Updated dictionary with new PD

--- config/dijetTLA/InitialParameters.py
+++ config/dijetTLA/InitialParameters.py
@@ -53,12 +53,12 @@ initialParameters = {
 	'J100' : {  	
 	  
 	  'nbkg' 	: "4E7,0,1E8",
-	  'datafile'	: "Input/data/dijetTLA/unblinding2_mjj_spectra_all.root",
+	  'datafile'	: "Input/data/dijetTLA/unblinding2_mjj_spectra_J100_no2017.root",
 	  'datahist'	: "L1J100/Mjj_1GeVbinning",
 	  # Analysis range		
-	  'low'  : 457,
+	  'low'  : 457,  # 481 -- trying one less bin, because of the mjj turnon threshold is now a bit higher.
 	  'high' : 2997,
-	  'lumi' : 29.3,
+	  'lumi' : 19.6,  #29.5 - 9.869 (2017)
 	},
 	
 	'J50'  : {	
@@ -67,10 +67,28 @@ initialParameters = {
 	  'datafile'	: "Input/data/dijetTLA/unblinding2_mjj_spectra_all.root",
 	  'datahist'	: "L1J50/Mjj_1GeVbinning",
 	  'lumi'	: 1.5,
-	  'low'  : 302,
+	  'low'  : 282,  # 282 --trying one more bin, close to the mjj 99.9 threshold, but below, DIDN'T WORK
 	  'high' : 1516, 
 	},
+    },
 
+    'partialDataset2_PD' : { # PD based on partialDataset2
+
+	'J100' : {
+	  'four' 	: "Input/data/dijetTLA/PD_Run2_GlobalFit_457_2997_fivePar_finebinned_J100_scale6.78.root", 
+	  'five'	: "Input/data/dijetTLA/PD_Run2_GlobalFit_457_2997_sixPar_finebinned_J100_scale6.78.root", 
+	  'six' 	: "Input/data/dijetTLA/PD_Run2_GlobalFit_457_2997_sevenPar_finebinned_J100_scale6.78.root",
+	  
+	  #'four'	: "Input/data/dijetTLA/PD_Run2_GlobalFit_481_2997_fivePar_finebinned_J100_scale6.78.root", 
+	  #'five'	: "Input/data/dijetTLA/PD_Run2_GlobalFit_481_2997_sixPar_finebinned_J100_scale6.78.root", 
+	  #'six'  	: "Input/data/dijetTLA/PD_Run2_GlobalFit_481_2997_sevenPar_finebinned_J100_scale6.78.root",
+	  },
+
+	'J50'  : {
+	  'four' 	: "Input/data/dijetTLA/PD_Run2_GlobalFit_302_1516_fivePar_finebinned_J50Comb_scale9.77.root",
+	  'five'	: "Input/data/dijetTLA/PD_Run2_GlobalFit_302_1516_sixPar_finebinned_J50Comb_scale9.77.root",	
+	  'six' 	: "Input/data/dijetTLA/PD_Run2_GlobalFit_302_1516_sevenPar_finebinned_J50Comb_scale9.77.root",
+	  },
     },
 
     'full' : {  # Full lumi: total datafile
